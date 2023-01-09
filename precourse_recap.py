1cca41d298499d2eef1e418796270441453a9c78
Graeme Scott <graemesco96@gmail.com>
2023-01-09 16:12:21 +0000

    first commit

---
+++ precourse_recap.py
@@ -0,0 +1,25 @@
+python3
+
+user = "graeme Scott"
+squad = " CodeClan!" 
+
+type("graeme Scott")
+
+user.capitalize()
+
+int_string = "4"
+
+int_string_result = int("4") - 2
+
+my_thoughts = user + ( " is enjoying ") + squad
+
+print(my_thoughts)
+
+int_result = 5 * 6
+print(int_result)
+
+float_result = 5.5 - 3.5
+print(float_result)
+
+dividing_result = 10 / 5
+print(dividing_result)
